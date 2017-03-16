a4fcf2f0983374745d51d06d9b1db3755c1ac470
kaihalfinger <Kai.Halfinger@gmail.com>
2017-03-16 17:21:48 +0100

    Variable „name“ in die Funktion eingefügt

--- Aufgabe-0/js/aufgabe0.ts
+++ Aufgabe-0/js/aufgabe0.ts
@@ -1,6 +1,5 @@
-var name : string;
-
 document.addEventListener('DOMContentLoaded', function() {
+	var name : string;
     name = prompt("Bitte geben Sie Ihren Namen ein"); 
     document.getElementById("greeting").innerHTML += name; 
 });
