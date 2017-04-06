c10fe2f5a3b61ff1e75b1dcf82bb37f8dea14eaf
kaihalfinger <Kai.Halfinger@gmail.com>
2017-04-07 01:50:31 +0200

    Funktioniert, bis auf die Erkennung des geklickten Divs anhand einer ID

--- Aufgabe-3b/js/script.js
+++ Aufgabe-3b/js/script.js
@@ -8,32 +8,35 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 */
 document.addEventListener('DOMContentLoaded', function () {
     var karten = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo As", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz As", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz As", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik As"];
-    var gezogeneKarten = 0;
-    var abgelegteKarten = 0;
-    var uebrigeKarten = karten.length - gezogeneKarten;
+    var handKarten = [];
+    var abgelegteKarten = [];
     document.getElementById("nachziehstapel").addEventListener("click", function () {
-        if (gezogeneKarten < 5 && uebrigeKarten > 0) {
+        if (handKarten.length < 5 && karten.length > 0) {
             var kartennummer = Math.floor((Math.random() * 31) + 0); // Generiert 1 zufällige Zahl zwischen 0 und 31
             while (karten[kartennummer] == undefined) {
                 kartennummer = Math.floor((Math.random() * 31) + 0);
             }
-            var kartenwert = karten[kartennummer]; // "Nimmt" 1 Karte aus dem Array
-            karten.splice(kartennummer, 1); // entfernt die gezogene Karte aus dem Array
-            var div = document.createElement("div"); // Erstellt 1 Div zur Darstellung der gezogenen Karte
+            handKarten.push(karten[kartennummer]); // fügt die Karte dem Hand Array hinzu
+            karten.splice(kartennummer, 1); // entfernt die gewählte Karte aus dem Kartenstapel Array
+            var div = document.createElement("div"); // Erstellt 1 Div zur Darstellung der Hand Karte
             document.getElementById("hand").appendChild(div);
             var s = div.style;
             div.className = "handkarten";
-            div.textContent = kartenwert;
+            div.id = (handKarten.length - 1).toString();
+            div.textContent = handKarten[handKarten.length - 1];
+            console.log(handKarten);
+            console.log(karten);
             div.addEventListener("click", function () {
-                gezogeneKarten--;
-                abgelegteKarten++;
-                document.getElementById("ablagestapel").textContent = "Ablagestapel" + "\r\n" + "Karten: " + abgelegteKarten.toString();
+                abgelegteKarten.push(handKarten[Number(this.id)]); // fügt die Karte dem Ablagestapel Array hinzu
+                handKarten.splice(Number(this.id), 1); // entfernte die geklickte Karte aus dem Array
+                console.log("Hand: " + handKarten);
+                console.log("Abgelegt: " + abgelegteKarten);
+                document.getElementById("ablagestapel").textContent = "Ablagestapel" + "\r\n" + "Oberste Karte: " + "\r\n" + abgelegteKarten[abgelegteKarten.length - 1] + "\r\n" + "\r\n" + "Karten: " + abgelegteKarten.length.toString();
                 document.getElementById("ablagestapel").style.display = "inline-block";
                 this.parentNode.removeChild(this); // Entfernt das Div
             });
-            gezogeneKarten++;
-            uebrigeKarten = 32 - gezogeneKarten - abgelegteKarten;
-            document.getElementById("nachziehstapel").textContent = "Kartenstapel" + "\r\n" + "Karten: " + uebrigeKarten.toString();
+            // handKarten++;
+            document.getElementById("nachziehstapel").textContent = "Kartenstapel" + "\r\n" + "Karten: " + karten.length.toString();
         }
         else { }
         ;
--- Aufgabe-3b/js/script.ts
+++ Aufgabe-3b/js/script.ts
@@ -10,35 +10,40 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 document.addEventListener('DOMContentLoaded', function() {
 
 let karten : string[] = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo As", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz As", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz As", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik As"];
-let gezogeneKarten : number = 0;
-let abgelegteKarten : number = 0;
-let uebrigeKarten : number = karten.length - gezogeneKarten;
+let handKarten : string[] = [];
+let abgelegteKarten : string[] = [];
 
 document.getElementById("nachziehstapel").addEventListener("click", function() {
-	if(gezogeneKarten<5 && uebrigeKarten > 0) {
+	if(handKarten.length < 5 && karten.length > 0) {
 		let kartennummer : number = Math.floor((Math.random() * 31) + 0); // Generiert 1 zufällige Zahl zwischen 0 und 31
 		while(karten[kartennummer] == undefined) {	// Generiert 1 neue Zahl, wenn die Zahl bereits generiert wurde
 			kartennummer = Math.floor((Math.random() * 31) + 0);
 		}
-		let kartenwert : string = karten[kartennummer]; // "Nimmt" 1 Karte aus dem Array
-		karten.splice(kartennummer, 1); // entfernt die gezogene Karte aus dem Array
+		handKarten.push(karten[kartennummer]); // fügt die Karte dem Hand Array hinzu
+		karten.splice(kartennummer, 1); // entfernt die gewählte Karte aus dem Kartenstapel Array
 
-		let div: HTMLDivElement = document.createElement("div"); // Erstellt 1 Div zur Darstellung der gezogenen Karte
+		let div: HTMLDivElement = document.createElement("div"); // Erstellt 1 Div zur Darstellung der Hand Karte
 		document.getElementById("hand").appendChild(div);
 		let s: CSSStyleDeclaration = div.style;
-		div.className = "handkarten";
-		div.textContent = kartenwert;
-		div.addEventListener("click", function() {
-			gezogeneKarten--;
-			abgelegteKarten++;
-			document.getElementById("ablagestapel").textContent = "Ablagestapel" + "\r\n" + "Karten: " + abgelegteKarten.toString();
-			document.getElementById("ablagestapel").style.display = "inline-block";
-			this.parentNode.removeChild(this); // Entfernt das Div
-		})
+			div.className = "handkarten";
+			div.id = (handKarten.length-1).toString();
+			div.textContent = handKarten[handKarten.length-1];
+			console.log(handKarten);
+			console.log(karten);
 
-		gezogeneKarten++;
-		uebrigeKarten = 32 - gezogeneKarten - abgelegteKarten;
-		document.getElementById("nachziehstapel").textContent = "Kartenstapel" + "\r\n" + "Karten: " + uebrigeKarten.toString();
+
+			div.addEventListener("click", function() {
+				abgelegteKarten.push(handKarten[Number(this.id)]); // fügt die Karte dem Ablagestapel Array hinzu
+				handKarten.splice(Number(this.id), 1); // entfernte die geklickte Karte aus dem Array
+				console.log("Hand: " + handKarten);
+				console.log("Abgelegt: " + abgelegteKarten);
+				document.getElementById("ablagestapel").textContent = "Ablagestapel" + "\r\n" + "Oberste Karte: " + "\r\n" + abgelegteKarten[abgelegteKarten.length-1] + "\r\n" + "\r\n" + "Karten: " + abgelegteKarten.length.toString();
+				document.getElementById("ablagestapel").style.display = "inline-block";
+				this.parentNode.removeChild(this); // Entfernt das Div
+			})
+
+		// handKarten++;
+		document.getElementById("nachziehstapel").textContent = "Kartenstapel" + "\r\n" + "Karten: " + karten.length.toString();
 	}
 	else {};	
 });
