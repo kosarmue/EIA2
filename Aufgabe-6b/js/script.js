66b162bc960161e0ca3256b278f671b8d5c90584
kaihalfinger <Kai.Halfinger@gmail.com>
2017-05-07 18:31:44 +0200

    Eingaben werden geprüft

--- Aufgabe-6b/js/script.js
+++ Aufgabe-6b/js/script.js
@@ -7,7 +7,7 @@ var StudiVZ;
         switch (action) {
             case "n":
             case "N":
-                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 (= weiblich) oder 1(= männlich)) und Kommentar");
                 alert(saveData(input));
                 break;
             case "a":
@@ -22,6 +22,38 @@ var StudiVZ;
     }
     function saveData(_input) {
         var array = _input.split(", ");
+        if (array.length != 6) {
+            return "Bitte geben Sie alle erforderlichen Informationen ein";
+        } // Alle Daten eingegeben?
+        if (isNaN(parseInt(array[0]))) {
+            return "Die Matrikelnummer muss als Zahl eingegeben werden";
+        } // Matrikelnr = Zahl?
+        if (array[1].match(/^[a-zA-Z]+$/)) { }
+        else {
+            return "Der Name darf nur aus Buchstaben bestehen";
+        } // Name = Buchstaben?
+        if (/^[a-zA-Z]+$/.test(array[2])) { }
+        else {
+            return "Der Vorname darf nur aus Buchstaben bestehen";
+        } // Vorname = Buchstaben?
+        if (isNaN(parseInt(array[3]))) {
+            return "Das Alter muss als Zahl eingegeben werden";
+        } // Alter = Zahl?
+        if (parseInt(array[4]) != 0 && parseInt(array[4]) != 1) {
+            return "Das Geschlecht muss mit '0' (weiblich) oder '1' (männlich) angegeben werden";
+        } // Geschlecht = 0 oder 1?
+        for (var i = 0; i < students.length; i++) {
+            if (students[i].matrikelnr == parseInt(array[0])) {
+                var geschlecht_1 = void 0;
+                if (students[i].geschlecht == true) {
+                    geschlecht_1 = "m";
+                }
+                else {
+                    geschlecht_1 = "w";
+                }
+                return "Für diese Matrikelnummer gibt es bereits einen Eintrag:\nMatrikelnr: " + students[i].matrikelnr + "\nName: " + students[i].name + "\nVorname: " + students[i].vorname + "\nAlter: " + students[i].alter + "\nGeschlecht: " + geschlecht_1 + "\nKommentar: " + students[i].kommentar;
+            }
+        }
         var s = {
             matrikelnr: parseInt(array[0]),
             name: array[1],
@@ -30,8 +62,8 @@ var StudiVZ;
             geschlecht: parseInt(array[4]) == 1,
             kommentar: array[5]
         };
-        students.push(s);
-        var geschlecht;
+        students.push(s); // Fügt StudentData dem array students hinzu
+        var geschlecht; // Wandelt 0 und 1 in das entsprechende Geschlecht um (für die Ausgabe)
         if (s.geschlecht == true) {
             geschlecht = "m";
         }
@@ -52,8 +84,8 @@ var StudiVZ;
                 }
                 return "Matrikelnr: " + students[i].matrikelnr + "\nName: " + students[i].name + "\nVorname: " + students[i].vorname + "\nAlter: " + students[i].alter + "\nGeschlecht: " + geschlecht + "\nKommentar: " + students[i].kommentar;
             }
-            else { }
         }
+        return "Für diese Matrikelnummer gibt es keinen Eintrag";
     }
 })(StudiVZ || (StudiVZ = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe-6b/js/script.ts
+++ Aufgabe-6b/js/script.ts
@@ -16,7 +16,7 @@ namespace StudiVZ {
         switch (action) {
             case "n":
             case "N":
-                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 (= weiblich) oder 1(= männlich)) und Kommentar");
                 alert(saveData(input));
                 break;
             case "a":
@@ -32,23 +32,41 @@ namespace StudiVZ {
 
     function saveData(_input: string): string {
         let array : string[] = _input.split(", ");
-        let s: StudentData = {
+        if (array.length != 6) { return "Bitte geben Sie alle erforderlichen Informationen ein"; } // Alle Daten eingegeben?
+        if (isNaN(parseInt(array[0]))) { return "Die Matrikelnummer muss als Zahl eingegeben werden"; } // Matrikelnr = Zahl?
+        if (array[1].match(/^[a-zA-Z]+$/)) {} else { return "Der Name darf nur aus Buchstaben bestehen"; } // Name = Buchstaben?
+        if (/^[a-zA-Z]+$/.test(array[2])) {} else { return "Der Vorname darf nur aus Buchstaben bestehen"; } // Vorname = Buchstaben?
+        if (isNaN(parseInt(array[3]))) { return "Das Alter muss als Zahl eingegeben werden"; } // Alter = Zahl?
+        if (parseInt(array[4]) != 0 && parseInt(array[4]) != 1) { return "Das Geschlecht muss mit '0' (weiblich) oder '1' (männlich) angegeben werden"; } // Geschlecht = 0 oder 1?
+        for (let i : number = 0; i < students.length; i++) { // Matrikelnr bereits vorhanden?
+            if (students[i].matrikelnr == parseInt(array[0])) {
+                let geschlecht: string;
+                if (students[i].geschlecht == true) {
+                    geschlecht = "m";
+                }
+                else {
+                    geschlecht = "w";
+                }
+                return "Für diese Matrikelnummer gibt es bereits einen Eintrag:\nMatrikelnr: " + students[i].matrikelnr + "\nName: " + students[i].name + "\nVorname: " + students[i].vorname + "\nAlter: " + students[i].alter + "\nGeschlecht: " + geschlecht + "\nKommentar: " + students[i].kommentar ;
+            }
+        }
+        let s: StudentData = { // Schreibt die Daten in StudentData
             matrikelnr: parseInt(array[0]),
             name: array[1],
             vorname: array[2],
             alter: parseInt(array[3]),
             geschlecht: parseInt(array[4]) == 1,
             kommentar: array[5]
-        };
-        students.push(s);
+        };   
+        students.push(s); // Fügt StudentData dem array students hinzu
 
-       let geschlecht: string;
-       if (s.geschlecht == true) {
-           geschlecht = "m";
-       }
-       else {
-           geschlecht = "w";
-       }
+        let geschlecht: string; // Wandelt 0 und 1 in das entsprechende Geschlecht um (für die Ausgabe)
+        if (s.geschlecht == true) {
+            geschlecht = "m";
+        }
+        else {
+            geschlecht = "w";
+        }
         return "Daten gespeichert\nMatrikelnr: " + s.matrikelnr + "\nName: " + s.name + "\nVorname: " + s.vorname + "\nAlter: " + s.alter + "\nGeschlecht: " + geschlecht + "\nKommentar: " + s.kommentar;
     }
 
@@ -64,7 +82,7 @@ namespace StudiVZ {
                 }
                 return "Matrikelnr: " + students[i].matrikelnr + "\nName: " + students[i].name + "\nVorname: " + students[i].vorname + "\nAlter: " + students[i].alter + "\nGeschlecht: " + geschlecht + "\nKommentar: " + students[i].kommentar;
             }
-            else{}
         }
+        return "Für diese Matrikelnummer gibt es keinen Eintrag";
     }
 }
